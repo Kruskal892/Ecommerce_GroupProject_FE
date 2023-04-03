2fcd9954be0c6cb14f0c98c6f6dfe95c68b64100
Your Name <you@example.com>
2023-04-03 23:50:57 +0700

    Login & Logout implementation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserAuthService } from '../services/user-auth.service';
+import { Router } from '@angular/router';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  constructor(
+    private userAuthService: UserAuthService,
+    private router: Router,
+    public userService: UserService
+  ) {}
 
+  ngOnInit(): void {}
+
+  public isLoggedIn() {
+    return this.userAuthService.isLoggedIn();
+  }
+
+  public logout() {
+    this.userAuthService.clear();
+    this.router.navigate(['/home']);
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -20,23 +20,21 @@ export class LoginComponent implements OnInit {
 
   login(loginForm: NgForm) {
     this.userService.login(loginForm.value).subscribe(
-      (response:any) => {
-        const role = response.user.role.roleName;
-        this.userAuthService.setRoles(role);
+      (response: any) => {
+        
+        this.userAuthService.setRoles(response.user.role);
         this.userAuthService.setToken(response.jwtToken);
 
-        console.log(response)
+        const role = response.user.role[0].roleName;
 
-        if(role === 'Admin' ) {
-          this.router.navigate(['admin']);
-        }else{
-          this.router.navigate(['user']);
+        if (role === 'Admin') {
+          this.router.navigate(['/admin']);
+        } else {
+          this.router.navigate(['/user']);
         }
-        
       },
       (error) => {
         console.log(error);
-
       }
     );
   }
